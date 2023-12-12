95e5ed42e5469e5c9c3f88bea3818b813e2b1026
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-12 19:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans="";
+	for(let i=0;i<shortHex.length;i++){
+		if(shortHex[i] >= 97 && shortHex[i] <= 122){
+			ans+=shortHex[i]+shortHex[i];
+		}
+	}
+	return string.toUpperCase(shortHex) ? ans.toUpperCase : ans;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
